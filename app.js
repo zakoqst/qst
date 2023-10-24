93e3faf8d2aa64f8b11fce9288c7e0ebabac9960
zakoqst <148820410+zakoqst@users.noreply.github.com>
2023-10-24 15:01:29 +0200

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const path = require('path');
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use('images', express.static(path.join(__dirname, 'images')));
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use(session({ secret: 'secret-key', resave: true, saveUninitialized: true }));
 
