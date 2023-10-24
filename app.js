2d52cdd82aa333f109a5ed718ce1c27296815d82
zakoqst <148820410+zakoqst@users.noreply.github.com>
2023-10-24 14:01:14 +0200

    rating 3

--- app.js
+++ app.js
@@ -106,7 +106,7 @@ app.get('/', (req, res) => {
     <style>
         /* Your CSS styles here */
         /* ... */
-        .center {
+       .center {
             display: flex;
             justify-content: center;
             align-items: center;
@@ -115,21 +115,23 @@ app.get('/', (req, res) => {
         .rate-section {
             margin-top: 20px;
         }
+        .rating {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-direction: row;
+            font-size: 30px;
+        }
         .rating input {
             display: none;
         }
         .rating label {
-            display: inline-block;
-            font-size: 30px;
-            color: #ccc;
+            color: #ddd;
+            margin: 0 5px;
             cursor: pointer;
         }
-        .rating label:before {
-            content: '\\2606'; /* Unicode character for empty star */
-        }
-        .rating input:checked ~ label:before {
+        .rating input:checked ~ label {
             color: #ffca08; /* Color for the selected star */
-            content: '\\2605'; /* Unicode character for filled star */
         }
     </style>
 </head>
@@ -145,22 +147,47 @@ app.get('/', (req, res) => {
             <h3>Rate Me</h3>
             <p>Please provide your feedback by rating our service:</p>
             <div class="rating">
-                <input type="radio" id="star5" name="rating" value="5" />
-                <label for="star5">☆</label>
+       <input type="radio" id="star5" name="rating" value="5" />
+                <label for="star5">&#9733;</label>
                 <input type="radio" id="star4" name="rating" value="4" />
-                <label for="star4">☆</label>
+                <label for="star4">&#9733;</label>
                 <input type="radio" id="star3" name="rating" value="3" />
-                <label for="star3">☆</label>
+                <label for="star3">&#9733;</label>
                 <input type="radio" id="star2" name="rating" value="2" />
-                <label for="star2">☆</label>
+                <label for="star2">&#9733;</label>
                 <input type="radio" id="star1" name="rating" value="1" />
-                <label for="star1">☆</label>
+                <label for="star1">&#9733;</label>
             </div>
         </div>
         </form>
         <form action="/logout" method="post">
             <input type="submit" value="Logout">
         </form>
+
+
+          <script>
+        const ratingInputs = document.querySelectorAll('.rating input');
+        const labels = document.querySelectorAll('.rating label');
+
+        ratingInputs.forEach((input, index) => {
+            input.addEventListener('change', () => {
+                resetColors();
+                colorStars(index);
+            });
+        });
+
+        function resetColors() {
+            labels.forEach(label => {
+                label.style.color = '#ddd';
+            });
+        }
+
+        function colorStars(index) {
+            for (let i = 0; i <= index; i++) {
+                labels[i].style.color = '#ffca08';
+            }
+        }
+    </script>
         </body>
         </html>
     `;}
