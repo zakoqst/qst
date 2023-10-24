75921b164098befd7bd6f510e75d3dc068ab2c21
zakoqst <148820410+zakoqst@users.noreply.github.com>
2023-10-24 14:48:25 +0200

    static files images

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const fs = require('fs');
+const path = require('path');
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use('images', express.static(path.join(__dirname, 'images')));
 
 app.use(session({ secret: 'secret-key', resave: true, saveUninitialized: true }));
 
